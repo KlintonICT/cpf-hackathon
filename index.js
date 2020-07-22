4aa01b5d53bc470702add05bf6cd3617b2ef657c
Jirachai C <antronic.inc@gmail.com>
2020-07-23 03:16:44 +0700

    Remove static from express

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const app = express()
 
 const DEFAULT_PORT = 3000 || process.env.port || process.env.PORT
 
-app.use(express.static('dist'))
+// app.use(express.static('dist'))
+app.get('/', (req, res) => {
+  res.json({
+    msg: 'Hello',
+  })
+})
 
 app.listen(DEFAULT_PORT, () => {
     console.log('TRAGK app is listening on', DEFAULT_PORT)
