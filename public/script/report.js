00d28951fc386a2519ecfccf6caae51f0432d829
KlintonKraken <chhunklinton91@gmail.com>
2020-07-23 04:23:38 +0700

    add report page

--- public/script/report.js
+++ public/script/report.js
@@ -1,20 +1,18 @@
-// (() => {
-//     const displayDateRange = () => {
-//         $('input[name="daterange"]').daterangepicker({
-//             opens: 'right'
-//           }, function(start, end, label) {
-//             console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-//         });
-//         // $('input[name="daterange"]').daterangepicker({
-//         //     "startDate": "07/12/2020",
-//         //     "endDate": "07/18/2020"
-//         // }, function(start, end, label) {
-//         //   console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
-//         // });
-//     }
+(() => {
+    const displayDateRange = () => {
+        const now = new Date();
+        const startDate = (now.getMonth()) + '/' + now.getDate() + '/' + now.getFullYear();
+        const endDate = (now.getMonth()+1) + '/' + now.getDate() + '/' + now.getFullYear();
+        $('#daterange').val(startDate + ' - ' + endDate);
+        $('input[name="daterange"]').daterangepicker({
+            opens: 'left'
+        }, function(start, end, label) {
+            console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+        });
+    }
 
-//     const run = () => {
-//         displayDateRange();
-//     }
-//     run();
-// })();
+    const run = () => {
+        displayDateRange();
+    }
+    run();
+})();
