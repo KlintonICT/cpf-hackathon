ad598b548631c5b2897da1436ac5894f3dd1c53c
Ampere J <ampere@Amperes-MacBook-Pro.local>
2020-07-22 20:43:17 +0700

    Add initial gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,24 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 
 const scss = (cb) => {
-    return gulp.src('./src/scss/**/*.scss')
+    gulp.src('./src/scss/**/*.scss')
       .pipe(sass()) // Using gulp-sass
       .pipe(gulp.dest('./public/styles'))
+
+    cb()
 }
 
-const moveFile = (cb) => {
-    return gulp.src('./public/**/*')
+const moveFiles = (cb) => {
+    gulp.src('./public/**/*')
         .pipe(gulp.dest('./dist'))
+    cb()
 }
 
 // Export main task
 exports.default = () => {
+    // For initial task
+    gulp.series(scss)()
+    
     gulp.watch('./src/scss/**/*.scss', gulp.parallel(scss))
-    gulp.watch('./public/**/*', gulp.series(moveFile))
+    gulp.watch('./public/**/*', gulp.series(moveFiles))
 }
